63e4ab8394f4ed3d1923d1966a5efbc92d5c5a0f
pereag <perello.valentin@hotmail.fr>
2018-02-07 23:52:43 +0100

    error bootstrap in header and footer fixed

--- src/view/backend/footer.php
+++ src/view/backend/footer.php
@@ -1,6 +1,6 @@
-<footer class="row">
+<footer>
 	<div class="col-lg-12" id="administration_space">
-		<div class="col-lg-offset-2 col-lg-4">
+		<div class="col-lg-offset-1 col-lg-4">
 			<ul class="list-unstyled">
 				<a href="index.php"><li>Déconnexion</li></a><br/> 
 				<li>Copyright © 2018 – Jean Forteroche</li><br/>
--- src/view/backend/header.php
+++ src/view/backend/header.php
@@ -1,5 +1,5 @@
 <header class="row">
-	<div class="col-lg-2 col-md-3 col-sm-3 col-xs-7">	
+	<div class="col-lg-3 col-md-3 col-sm-3 col-xs-7">	
 		<a href="index.php?action=listPostsView"><img src="public/picture/logo_jean.png"></a>
 	</div>
 </header>
