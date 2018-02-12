7be7253b9fe56f32db1d044599d4b83e541df708
pereag <perello.valentin@hotmail.fr>
2018-02-12 13:44:03 +0100

    add twig 1.35.0 via composer

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -6,9 +6,36 @@ namespace Composer\Autoload;
 
 class ComposerStaticInit278e2dd4a8cbac8b097ab989322d0d98
 {
+    public static $prefixLengthsPsr4 = array (
+        'T' => 
+        array (
+            'Twig\\' => 5,
+        ),
+    );
+
+    public static $prefixDirsPsr4 = array (
+        'Twig\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/twig/twig/src',
+        ),
+    );
+
+    public static $prefixesPsr0 = array (
+        'T' => 
+        array (
+            'Twig_' => 
+            array (
+                0 => __DIR__ . '/..' . '/twig/twig/lib',
+            ),
+        ),
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
+            $loader->prefixLengthsPsr4 = ComposerStaticInit278e2dd4a8cbac8b097ab989322d0d98::$prefixLengthsPsr4;
+            $loader->prefixDirsPsr4 = ComposerStaticInit278e2dd4a8cbac8b097ab989322d0d98::$prefixDirsPsr4;
+            $loader->prefixesPsr0 = ComposerStaticInit278e2dd4a8cbac8b097ab989322d0d98::$prefixesPsr0;
 
         }, null, ClassLoader::class);
     }
