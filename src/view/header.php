a5d1e0b3b8ca3222d3c6fd26ac7536698ea3d976
pereag <perello.valentin@hotmail.fr>
2018-02-13 01:22:36 +0100

    modif header for a best mobil experience

--- src/view/header.php
+++ src/view/header.php
@@ -1,5 +1,7 @@
 <header class="row">
-	<div class="col-lg-3 col-md-3 col-sm-3 col-xs-7">	
-		<a href="index.php?action=listPostsView"><img src="public/picture/logo_jean.png"></a>
+	<div class ="col-lg-9">
+		<div class="col-lg-3 col-md-3 col-sm-3 col-xs-6">	
+			<a href="index.php?action=listPostsView"><img src="public/picture/logo_jean.png"></a>
+		</div>
 	</div>
 </header>
