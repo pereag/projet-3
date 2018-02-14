d96122fca33c22eb0338663df860797f334ef839
pereag <perello.valentin@hotmail.fr>
2018-02-14 11:29:49 +0100

    delete bootstrap in public and instal via composer in vendor

--- src/view/template.php
+++ src/view/template.php
@@ -2,12 +2,11 @@
 <html>
 <head>
 	<title><?= $title ?></title>
-	<link rel="stylesheet" type="text/css" href="public/bootstrap-3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="vendor/twbs/bootstrap/dist/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="/projet_3/public/css/style.css">
 	<link rel="icon" type="image/png" href="/projet_3/public/picture/iconj.png" />
 	<link rel="shortcut icon" type="image/x-icon" href="/projet_3/public/picture/iconj.ico" />
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="public/bootstrap-3.3.7/js/bootstrap.min.js"></script>
 	<script src="vendor/tinymce/tinymce/tinymce.min.js"></script>
 	<script>tinymce.init({ selector:'#textarea' });</script>
 </head>
